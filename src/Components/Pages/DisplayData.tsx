84bbd62b756949d2ddca09b40bcecb87cb3bdefa
SaiSrinivas321 <gssrinivas32194@gmail.com>
2021-11-02 18:26:04 +0530

    page to display submitted data

--- src/Components/Pages/DisplayData.tsx
+++ src/Components/Pages/DisplayData.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "../../Styles/displayData.css"
 
 interface Probs {}
 
 export const DisplayData = (probs: Probs) => {
-  return <div>Display Data</div>;
+  return(
+    <div className="displayData-div">
+      <div className="displaydata-heading">Your Submission</div>
+      <table className="styled-table">
+      <tbody>
+          <tr>
+              <td>Location </td>
+              <td>............</td>
+          </tr>
+          <tr>
+              <td>Flood Depth</td>
+              <td>..............</td>
+          </tr>
+          <tr>
+              <td>Image Uploaded</td>
+              <td>............</td>
+          </tr>
+      </tbody>
+  </table>
+
+    </div>
+  );
 };
