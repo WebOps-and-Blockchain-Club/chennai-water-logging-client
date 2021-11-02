719ff53c11c8e98af3a906c3197770f752c2ebe4
SaiSrinivas321 <gssrinivas32194@gmail.com>
2021-11-02 18:25:39 +0530

    added geolocation and Image upload

--- src/Components/Pages/AddData.tsx
+++ src/Components/Pages/AddData.tsx
@@ -1,43 +1,56 @@
 import React from "react";
-import "../../Styles/addData.css"
+import "../../Styles/addData.css";
 
 interface Probs {}
 
 export const AddData = (probs: Probs) => {
   const [filelist , setFilelist] = React.useState();
-  const [latitude , setLatitude] = React.useState(0);
-  const [longitude , setLongitude] = React.useState(0);
+  const [location , setLocation] = React.useState<any >();
  
   const [error , setError] = React.useState<any>()
+
   const handleImageUpload = ( e : any) =>{
       setFilelist(e.target.files[0])
   }
 
+
   const getGeolocation = (e : any)=>{
     e.preventDefault();
+    let coords = {
+      latitude : 0 ,
+      longitude : 0 ,
+      location : " "
+    };
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        console.log(navigator)
-        setLatitude(position.coords.latitude)
-        setLongitude(position.coords.longitude)
-      },err => setError(err.message));
-    }else{
-      setLatitude(100000)
+      navigator.geolocation.getCurrentPosition((res)=>{
+        coords.latitude = res.coords.latitude;
+        coords.longitude = res.coords.longitude;
+        coords.location = "Location";
+        setLocation(coords)
+
+      });
+    } else {
+      // x.innerHTML = "Geolocation is not supported by this browser.";
     }
+  
   }
- 
+  
         return(
           <form >
-        <label>Geo Location</label>
-        <button onClick={getGeolocation}>Get location</button>
-        <label>Latitude : {latitude}  Longitude : {longitude}</label>
-        <label>{error}</label>
-        <label>Flood depth</label>
-        <input name="address" />
-        <label>Image to support the request</label>
-        <input type="file" accept="image/*" capture="environment" onChange={handleImageUpload}/>
-        {/* <label>{filelist}</label> */}
-        <button type="submit" className="submitButton">Submit</button>
-      </form>
+          <label>Geo Location</label>
+          <button onClick={getGeolocation} className="locationButton">Get location</button>  
+          <label>{location ? 
+           <a href={`https://www.google.com/maps/search/?api=1&query=${location.latitude},${location.longitude}`} target="_blank">Check Location</a> : null}</label>
+          <label>Flood depth</label>
+          <select id="depth">
+            <option value="0-5">0-5 cm</option>
+            <option value="5-10">5 -10 cm</option>
+            <option value="10-15">10-15 cm</option>
+            <option value="15">15+ cm</option>
+          </select>
+          <label>Image to support the request</label>
+          <input type="file" accept="image/*" capture="environment" onChange={handleImageUpload}/>
+          <button type="submit" className="submitButton">Submit</button>
+          </form>
         )
 };
